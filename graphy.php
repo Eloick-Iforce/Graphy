323d4b5e2eb364bbc38baa620da1cc657d953a53
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-07-19 09:40:07 +0200

    init

---
+++ graphy.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Graphy
+Description: Un plugin pour créer des graphiques
+Version: 1.0
+Author: Eloïck Mickisz
+*/
+
+add_action('admin_menu', 'graphy_menu');
+
+function graphy_menu()
+{
+    add_menu_page(
+        'Graphy',
+        'Graphy',
+        'manage_options',
+        'graphy',
+        'graphy_page',
+        'dashicons-admin-generic',
+        6
+    );
+}
+
+function graphy_page()
+{
+    require 'pages/index.php';
+}
---
+++ pages/index.php
@@ -0,0 +1,7 @@
+    <script src="https://cdn.tailwindcss.com"></script>
+
+
+    <div class="flex flex-col gap-8 m-8">
+        <h1 class="text-4xl font-bold">Graphy</h1>
+        <p>Bon retour sur Graphy !</p>
+    </div>
