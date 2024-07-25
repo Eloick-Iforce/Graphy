8d448ae06ec927b2d1958d9706634959f6c05d4b
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-07-25 10:53:13 +0200

    chore: Update chart container styling and add lazy loading for chart images

--- pages/index.php
+++ pages/index.php
@@ -28,16 +28,36 @@
                                 var chartData = JSON.parse(<?php echo wp_json_encode($chart['dataset_data']); ?>);
                                 var chartType = JSON.parse(<?php echo wp_json_encode($chart['dataset_type']); ?>);
 
+                                var colors = [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)',
+                                    'rgba(75, 192, 192, 0.2)',
+                                    'rgba(153, 102, 255, 0.2)',
+                                    'rgba(255, 159, 64, 0.2)'
+                                ];
+                                var borderColor = [
+                                    'rgba(255, 99, 132, 1)',
+                                    'rgba(54, 162, 235, 1)',
+                                    'rgba(255, 206, 86, 1)',
+                                    'rgba(75, 192, 192, 1)',
+                                    'rgba(153, 102, 255, 1)',
+                                    'rgba(255, 159, 64, 1)'
+                                ];
+
                                 new Chart(ctx, {
                                     type: chartType[0],
                                     data: {
                                         labels: chartData[0].labels,
-                                        datasets: chartData.map(function(dataset) {
+                                        datasets: chartData.map(function(dataset, index) {
+                                            var backgroundColors = dataset.data.map((_, i) => colors[i % colors.length]);
+                                            var borderColors = dataset.data.map((_, i) => borderColor[i % borderColor.length]);
+
                                             return {
                                                 label: dataset.name,
                                                 data: dataset.data,
-                                                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                                                borderColor: 'rgba(75, 192, 192, 1)',
+                                                backgroundColor: backgroundColors,
+                                                borderColor: borderColors,
                                                 borderWidth: 1,
                                                 fill: false
                                             };
