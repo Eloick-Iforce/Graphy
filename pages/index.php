d8f67d5151e1c8d8d60407d8c9a19e06500c50e9
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-07-25 10:52:06 +0200

    chore: Update chart container styling and add lazy loading for chart images

--- pages/index.php
+++ pages/index.php
@@ -36,6 +36,7 @@
                                             return {
                                                 label: dataset.name,
                                                 data: dataset.data,
+                                                backgroundColor: 'rgba(75, 192, 192, 0.2)',
                                                 borderColor: 'rgba(75, 192, 192, 1)',
                                                 borderWidth: 1,
                                                 fill: false
